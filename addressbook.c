df8861fc768775b47809650a394a96356340f20e
elakamarcus <elakamarcus@users.noreply.github.com>
2018-12-02 00:36:40 +0800

    Update addressbook.c

--- addressbook.c
+++ addressbook.c
@@ -12,69 +12,36 @@ or use control var
 
 #define MAXCONTACTS 5
 
-struct contact {
+typedef struct contact {
     char firstname[50];
     char lastname[50];
     char phone[15];
     char email[20];
-    int priority;
-};
+} contact;
 
-
-void menu();
-struct contact createContact( void );
-void removeContact( struct contact *ctptr);
-int getNumContacts( struct contact *contacts[MAXCONTACTS]);
-void printcontact( struct contact *b );
-
-int main() {
-
-    struct contact contacts[MAXCONTACTS] = {0};
-    int i,count;
-    count = 0;
-    do {
-        menu();
-        printf("\nInput your selection: ");
-        scanf("%d", &i);
-        if(i==1){
-                /* should first find.
-                1. if contacts are full.
-                2. which is next available.
-                3. consider send the whole array.
-                 */
-           contacts[count] = createContact();
-           count += 1;
-        }
-        else if(i==2){
-            printcontact(&contacts[count]);
-        }
-        else if(i==3){
-            printf("Which contact to remove?\n");
-            listContacts(&contacts);
-
-        }
-
-    }while(i!=4);
-    if(i==4){
-        printf("End program. Press any key.");
-    }
-    return;
-}
+contact contacts[MAXCONTACTS];
 
 void menu(){
-
-    printf("1. Add a contact\n");
+    printf("Make your selection:\n");
+    printf("1. Create contacts\n");
     printf("2. List contacts\n");
-    printf("3. Remove a contact\n");
-    printf("4. Quit\n");
+    printf("3. Quit\n");
 
 }
 
-struct contact createContact( void ) {
+void printAllContact(contact* allcont, int n){
+    for(int i=0; i< n; i++){
+        printf("Contact: %d\n", i+1);
+        printf("\t%s, %s\n", allcont[i].lastname, allcont[i].firstname);
+        printf("\t%s\n", allcont[i].email);
+        printf("\t%s\n", allcont[i].phone);
+    }
+    printf("Size of allcont = %d\n", sizeof(allcont));
+}
+
+struct contact createContact( ) {
     struct contact ctptr;
     /* first find the first empty contact, or quit if the array is full */
-    int max = 0;
-    getchar( );
     printf("Enter the first name: ");
     fgets(ctptr.firstname, 50, stdin);
     ctptr.firstname[strlen(ctptr.firstname) - 1] = '\0';
@@ -87,28 +54,30 @@ struct contact createContact( void ) {
     printf("Enter email address: ");
     fgets(ctptr.email, 20, stdin);
     ctptr.email[strlen(ctptr.email) - 1] = '\0';
-    printf("Enter priority (1-100): ");
-    scanf("%d", &ctptr.priority);
+    getchar( ); /* remove \n from input buffer */
 
+    printf("Size of ctptr: %d\n", sizeof(ctptr));
     return ctptr;
 }
 
-void printcontact ( struct contact *b ){
-    printf("Contact: %s, %s\n", b->lastname, b->firstname );
-    printf("\tPhone: %s\n", b->phone); // pointer operator
-    printf("\tEmail: %s\n", b->email);
-    printf("\tPrio: %d\n", b->priority);
-
-}
-
-void removeContact( struct contact *ctptr){
-    printf("Not yet implemented.\n");
-
-}
-
-int getNumContacts( struct contact *contacts[MAXCONTACTS]) {
-    int max = MAXCONTACTS;
-    int b=0; // the 'length of array'
-
-    return b;
+int main(){
+    printf("Size of struct contact: %d\n", sizeof(struct contact));
+    int selection;
+    do {
+        menu();
+        scanf("%d", &selection);
+        getchar();
+        if(selection == 1){
+            for(int i=0; i<MAXCONTACTS; i++){
+            contacts[i] = createContact();
+            printf("Size of contacts: %d\n", sizeof(contacts));
+            }
+        }
+        else if(selection == 2){
+            printAllContact(contacts, MAXCONTACTS);
+        }
+    }while(selection <=2 && selection >=1);
+    if(selection==3){
+        printf("It's over.\n");
+    }
 }
